12b10795b0a025072fae57c4c5353f48d7c9fd40
Angélica Rodríguez <rodriguezq.angelicac@gmail.com>
2025-01-02 00:15:06 -0500

    Modificaciones del Display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -22,7 +22,7 @@ function Display() {
   return (
     <div
       ref={displayRef}
-      className="w-[100%] h-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
+      className="w-[100%] h-[90%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
     >
       <Routes>
         <Route path="/" element={<DisplayHome />} />
